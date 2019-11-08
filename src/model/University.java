7d2a09b3df10e5c6b6181429ed666f9cb51a55f5
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-11-08 10:39:40 -0500

    4

--- src/model/University.java
+++ src/model/University.java
@@ -1,6 +1,15 @@
 package model;
+import java.util.ArrayList;
 
 public class University {
-	//Relationships
+//Constant
+	public static final int nAuditoriums = 8;
+//Relationships
+	Auditorium auditoriums[];
+	ArrayList <Event> events;
 	
+	public University(){
+		auditoriums = new Auditorium[nAuditoriums];
+		events = new ArrayList<Event>();
+	}
 }
