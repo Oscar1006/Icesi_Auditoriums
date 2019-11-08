fad5ebec678b67d8253c4a1c354f1020ec20c85d
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-11-08 09:32:59 -0500

    Update Event.java

--- src/model/Event.java
+++ src/model/Event.java
@@ -1,16 +1,15 @@
 package model;
-<<<<<<< HEAD
 
 public class Event {
-	
+
 	private String name;
 	private String date;
-	private int startTime; 
-	private int endTime; 
-	private String teacher; 
+	private int startTime;
+	private int endTime;
+	private String teacher;
 	private String faculty;
 	private int people;
-	
+
 	public Event(String name, String date, int startTime, int endTime, String teacher, String faculty, int people) {
 		this.name = name;
 		this.date = date;
@@ -20,8 +19,5 @@ public class Event {
 		this.faculty = faculty;
 		this.people = people;
 	}
-	
-=======
-public class Event{
->>>>>>> a99d0151e3ea039a647254e57b89bd8d7b1416ba
+
 }
